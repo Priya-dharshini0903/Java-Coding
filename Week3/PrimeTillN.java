df50340ca702c7fdc5f0de8138bd343a4ab9b2a1
Priya-dharshini0903 <85675890+Priya-dharshini0903@users.noreply.github.com>
2025-05-27 06:35:29 +0530

    PrimeTillN.java

--- Week3/PrimeTillN.java
+++ Week3/PrimeTillN.java
@@ -1,8 +1,7 @@
 class Main {
     public static void main(String[] args) {
         int n=31;
-        System.out.print(2+" ");
-        for(int i=3;i<=n;i++){
+        for(int i=2;i<=n;i++){
             boolean isPrime=true;
            for(int j=2;j<i;j++){
             if((i%j)==0){
