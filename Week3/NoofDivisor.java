70c9659d62da46fab969a34b9780cce5ebbdc6d1
Priya-dharshini0903 <85675890+Priya-dharshini0903@users.noreply.github.com>
2025-05-27 06:48:20 +0530

    Create NoofDivisor.java

---
+++ Week3/NoofDivisor.java
@@ -0,0 +1,15 @@
+//Given an integer n, find the number of divisors of n that are divisible by 3.
+class Solution {
+    static int countDivisors(int n) {
+        // code here
+        int count=0;
+        for(int i=1;i<=n;i++){
+            if(n%i==0){
+                if(i%3==0){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
